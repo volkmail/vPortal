ab4c044fcbc73eb0fab5b15cb2ff7b0cd5f1bbe8
Volkov Aleksei <aleksV.dev@gmail.com>
2024-05-28 10:30:06 +1000

    2.16 Webpack hot module replacement

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buildDevServer(options: BuildOptions): WebpackDevServer.Configur
         port: options.port,
         open: true,
         historyApiFallback: true,
+        hot: true,
     } : undefined
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,5 +8,5 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
     return [new HtmlWebpackPlugin({template: paths.html}), new MiniCssExtractPlugin({
         filename: 'css/[name].[contenthash:8].css',
         chunkFilename: 'css/[name].[contenthash:8].css'
-    }), new webpack.DefinePlugin({__IS_DEV__: JSON.stringify(isDev),})]
+    }), new webpack.DefinePlugin({__IS_DEV__: JSON.stringify(isDev),}), new webpack.HotModuleReplacementPlugin()]
 }
