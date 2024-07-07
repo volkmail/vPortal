8c2172d3fc608aae31c1e0379ed447786c14909a
Volkov Aleksei <aleksV.dev@gmail.com>
2024-07-07 14:33:21 +1000

    2.20 Тестовая среда. Настраиваем Jest. Пишем первый тест

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   globals: {
     __IS_DEV__: true,
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,17 +1,18 @@
-type Mods = Record<string, boolean | string>;
+export type Mods = Record<string, boolean | string | undefined>;
 
-export const classNames = (
+export function classNames(
   cls: string,
-  mods?: Mods,
-  additional?: string[],
-): string =>
-  [
+  mods: Mods = {},
+  additional: Array<string | undefined> = [],
+): string {
+  return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
-      .filter(([className, value]) => Boolean(value))
-      .map(([className, value]) => className),
+    ...Object.entries(mods)
+      .filter(([_, value]) => Boolean(value))
+      .map(([className]) => className),
   ].join(' ');
+}
 
 // classNames('remove-btn', {hovered: true, selectable: true, red: false}, ['pdg'])
 // 'remove-btn hovered selectable pdg'
